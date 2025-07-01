77049258f646419284e17e0eb65eca2e9d1e4c9d
MD_AZAR <159606301+MD-AZAR17@users.noreply.github.com>
2025-07-01 05:45:13 -0700

    Alternative Series.java

---
+++ Alternative Series.java
@@ -0,0 +1,14 @@
+package training;
+public class Sample
+{
+	public static void main(String[] args) 
+  {
+		int j=1;
+		System.out.println("The series are : ");
+		for(int i=10;i>0;i--)
+		{
+			System.out.print(i+" "+j+" ");
+			j++;
+		}
+	}
+}
