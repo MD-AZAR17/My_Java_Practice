cacd954eaf963ed34f3def48badd773405d11ddb
MD_AZAR <159606301+MD-AZAR17@users.noreply.github.com>
2025-07-01 05:30:01 -0700

    Create Even series.java

---
+++ Even series.java
@@ -0,0 +1,16 @@
+package training;
+import java.util.Scanner;
+public class Sample
+{
+	public static void main(String[] args)
+  {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the range: ");
+		int n=sc.nextInt();
+		System.out.println("The even series are : ");
+		for(int i=2;i<=n;i+=2)
+		{
+			System.out.println(i);
+		}
+	}
+}
