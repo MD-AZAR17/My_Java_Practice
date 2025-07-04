7b811051d461bf60b098758be2916c2cc3aa5758
MD_AZAR <159606301+MD-AZAR17@users.noreply.github.com>
2025-07-04 20:36:10 +0530

    Create PrintCompanyName.java
    Get two string from user as input using Scanner class and add technologies in between company name and location

---
+++ PrintCompanyName.java
@@ -0,0 +1,15 @@
+package practice;
+import java.util.Scanner;
+public class program1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the first string: ");
+		String first=sc.nextLine();
+		System.out.println("Enter the Second string: ");
+		String second=sc.nextLine();
+		System.out.println(first + " Technologies " + second);
+
+	}
+
+}
